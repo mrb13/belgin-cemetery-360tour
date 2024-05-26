4aab74ee0af3b37f7941e45a6b74f5793f14c8c4
Marcus B <3343976+mrb13@users.noreply.github.com>
2024-05-26 09:53:05 -0400

    init commit
    descr: 1st commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# belgin-cemetery-360tour-
---
+++ data.js
@@ -0,0 +1,427 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-belgin-aerial-view",
+      "name": "Belgin aerial view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.23105127362145,
+        "pitch": 1.0325742127447555,
+        "fov": 1.3034465482088136
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4528601601703972,
+          "pitch": 0.250842495632579,
+          "rotation": 0,
+          "target": "5-rogers"
+        },
+        {
+          "yaw": -0.8637543981959723,
+          "pitch": 0.787826567098783,
+          "rotation": 0,
+          "target": "3-brewer"
+        },
+        {
+          "yaw": -2.005449951632267,
+          "pitch": 0.5847971029823356,
+          "rotation": 0,
+          "target": "6-row-a---1---frank"
+        },
+        {
+          "yaw": -2.202998250798739,
+          "pitch": 0.7701394600876803,
+          "rotation": 0,
+          "target": "7-row-a---2---francis"
+        },
+        {
+          "yaw": -2.8402253496972723,
+          "pitch": 1.0738094849058317,
+          "rotation": 0,
+          "target": "8-row-a---3---sarah"
+        },
+        {
+          "yaw": 2.5350408872547874,
+          "pitch": 1.0710846335702513,
+          "rotation": 0,
+          "target": "9-row-a---4---clara"
+        },
+        {
+          "yaw": -2.9797529092628814,
+          "pitch": 0.6255364616850372,
+          "rotation": 0,
+          "target": "1-big-headstone---ne"
+        },
+        {
+          "yaw": 0.21144037903269286,
+          "pitch": 0.30570551627173614,
+          "rotation": 0,
+          "target": "10-southeast-corner"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-big-headstone---ne",
+      "name": "Big Headstone - NE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-big-headstone---se",
+      "name": "Big Headstone - SE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-brewer",
+      "name": "Brewer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-entry",
+      "name": "Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-rogers",
+      "name": "Rogers",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.22526923639746954,
+        "pitch": 0.6822604454048307,
+        "fov": 1.3034465482088136
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.30892325023183,
+          "pitch": -0.6108325381601922,
+          "rotation": 0,
+          "target": "0-belgin-aerial-view"
+        },
+        {
+          "yaw": -1.5427390887968162,
+          "pitch": 0.23995809030890847,
+          "rotation": 0,
+          "target": "3-brewer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.48542609236895196,
+          "pitch": 0.6957190303439873,
+          "title": "James",
+          "text": "https://www.findagrave.com/memorial/23535746/james-rogers"
+        },
+        {
+          "yaw": -0.1694859829823674,
+          "pitch": 0.7080435331752959,
+          "title": "Leo",
+          "text": "https://www.findagrave.com/memorial/23535730/leo-rogers"
+        }
+      ]
+    },
+    {
+      "id": "6-row-a---1---frank",
+      "name": "Row A - 1 - Frank",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-row-a---2---francis",
+      "name": "Row A - 2 - Francis",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-row-a---3---sarah",
+      "name": "Row A - 3 - Sarah",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-row-a---4---clara",
+      "name": "Row A - 4 - Clara",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-southeast-corner",
+      "name": "Southeast Corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Belgin Cemetery",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
